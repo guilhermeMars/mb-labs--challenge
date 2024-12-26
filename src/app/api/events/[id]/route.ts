aa6c353244aed9021bd8485fd65ab044065d2980
Guilherme <guilhermemspiandorin@gmail.com>
2024-12-26 14:03:31 -0300

    fix: Tentativa de solucionar problema na hospedagem

--- src/app/api/events/[id]/route.ts
+++ src/app/api/events/[id]/route.ts
@@ -3,14 +3,17 @@ import eventData from "../../../../../db.json";
 import { Event } from "@/interface/eventInterface";
 
 const events: Event[] = eventData.events;
+
 export async function GET(
   req: Request,
   { params }: { params: { id: string } }
 ) {
-  const { id } = await params;
+  const { id } = params;
   const event = events.find((event) => event._id === id);
+
   if (!event) {
     return NextResponse.json({ message: "Event not found" }, { status: 404 });
   }
+
   return NextResponse.json(event);
 }
